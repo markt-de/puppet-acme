e4cf2ddf1de761bb1921adba0c3093f4535a5dda
Daniel Rieper <daniel.rieper@opal-consulting.com>
2019-01-28 12:13:28 +0100

    Fix: getvar Puppet 5 change

--- manifests/csr.pp
+++ manifests/csr.pp
@@ -186,7 +186,8 @@ define acme::csr(
     require => X509_request[$csr_file],
   }
 
-  $csr_content = pick_default(getvar("::acme_csr_${domain}"), '')
+  $domain_rep = regsubst(regsubst($domain, '\.', '_', 'G'),'-', '_', 'G')
+  $csr_content = pick_default(getvar("::acme_csr_${domain_rep}"), '')
   if ($csr_content =~ /CERTIFICATE REQUEST/) {
     @@acme::request { $domain:
       csr              => $csr_content,
