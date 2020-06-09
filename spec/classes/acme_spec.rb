57534305afb3d2b6fe64609d8db296e1e07ef667
Frank Wall <fw@moov.de>
2020-06-09 14:56:26 +0200

    add a basic unit test

--- spec/classes/acme_spec.rb
+++ spec/classes/acme_spec.rb
@@ -1,17 +1,61 @@
 require 'spec_helper'
 
-describe 'acme', type: :class do
-  let :facts do
-    {
-      osfamily: 'Debian',
-      fqdn: 'spectest',
-      puppetmaster: 'spectest',
-      acme_crts: 'spectest',
-      acme_csr_spectest: '-----BEGIN CERTIFICATE REQUEST-----\nMIICpzCCAY8CAQAwYjELMAkGA1UEBhMCQVQxETAPBgNVBAgMCFNhbHpidXJnMREw\nDwYDVQQHDAhTYWx6YnVyZzENMAsGA1UECgwEYnplZDEeMBwGA1UEAwwVc3BlYy10\nZXN0LmV4YW1wbGUuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA\nlc3jPhFUfrfPvvQOd7vtl1+eA4ak6MAEqGL0c6xjyIiaHffNMr1ujPhZIyDuX+a/\nvbB4en+FZrq3abSqGEF0+ca/aqluAxR3jQzM231g18UpAppceV/Xz8lOsk4u5vl2\nnBW/44GwpwV+rFTjgsNPZ3dDRWaTcyJ8FFxLstf5AWudecnLYEiWeXIpiCYiUZZH\neoO9SUhs77f2S3lU8sAeUGn8L4Xrx70cyhDym7gh2Vwf0LmvulsNvQQPPPEfh3Mp\nHQvhqPmRYVl/fAszEEhrElgwCT76LPbvJjs/bYdkDUbT1gv1Lv/4h7xbvc7YsIf9\naIGQGU+Tbc4Bpd1uMQjxwwIDAQABoAAwDQYJKoZIhvcNAQELBQADggEBAF8ytNzr\n2HLQaqjPH6ETOi3yiheJe8tNB1bV8YCtffxjPyIkRUONtMTuugD1bWSBz9ZsBIGs\nsTsUH1nBXUneinVjCBDM8pWGyyQJ+Sp9mGzO/ozgl/B3Ty54/J7OfE6kAlGibSoX\nQOmUGLdsRDGgVmismofs2fam4XDjijVxXMgrdzj22KbcuWmdF3W+9Sn8GpEXW4Nh\nT+oIq/TPAl/wym6jo6EeflMlOLo0V4Bb5ylmkPCkLVGwm/ClouxgNSRP/uDDa/kT\nMhRrz3+JfoRSK2PgzT1Ai48eTt98YxsBr261KvovbMbnpqPtCfedta33d7EESg32\nssyZggp6vUJnypw=\n-----END CERTIFICATE REQUEST-----'
-    }
+describe 'acme' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        context 'with example configuration' do
+          let :params do
+            {
+              accounts: ['certmaster@example.com', 'ssl@example.com'],
+              profiles: {
+                nsupdate_example: {
+                  challengetype: 'dns-01',
+                  hook: 'nsupdate',
+                  env: {
+                    NSUPDATE_SERVER: 'bind.example.com',
+                  },
+                  options: {
+                    dnssleep: 15,
+                    nsupdate_id: 'example-key',
+                    nsupdate_type: 'hmac-md5',
+                    nsupdate_key: 'abcdefg1234567890',
+                  },
+                },
+                route53_example: {
+                  challengetype: 'dns-01',
+                  hook: 'aws',
+                  env: {
+                    AWS_ACCESS_KEY_ID: 'foobar',
+                    AWS_SECRET_ACCESS_KEY: 'secret',
+                  },
+                  options: {
+                    dnssleep: 15,
+                  },
+                },
+              },
+            }
+          end
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('acme') }
+
+          it { is_expected.not_to contain_class('acme::setup::puppetmaster') }
+          it { is_expected.not_to contain_class('acme::request::handler') }
+          it { is_expected.not_to contain_class('acme::request::crt') }
+
+          it { is_expected.to contain_file('/etc/acme.sh').with_ensure('directory') }
+          it { is_expected.to contain_file('/etc/acme.sh/certs').with_ensure('directory') }
+          it { is_expected.to contain_file('/etc/acme.sh/keys').with_ensure('directory') }
+
+          it { is_expected.not_to contain_file('/etc/acme.sh/csrs').with_ensure('directory') }
+          it { is_expected.not_to contain_file('/etc/acme.sh/results').with_ensure('directory') }
+        end
+      end
+    end
   end
-  #  it { should contain_file('/etc/acme.sh').with_ensure('directory') }
-  #  it { should contain_file('/etc/acme.sh/certs').with_ensure('directory') }
-  #  it { should contain_file('/etc/acme.sh/csrs').with_ensure('directory') }
-  #  it { should contain_file('/etc/acme.sh/keys').with_ensure('directory') }
 end
